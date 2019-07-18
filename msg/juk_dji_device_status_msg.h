6bd09aeb862b922fb9a0acd26ca4066c2b95f721
Ubuntu <ubuntu@ubuntu.mshome.net>
2019-07-18 08:07:35 +0000

    upd

---
+++ msg/juk_dji_device_status_msg.h
@@ -0,0 +1,16 @@
+# authority constants
+uint8 CONTROL_BY_RC = 1
+uint8 CONTROL_BY_MOBILE = 2
+uint8 CONTROL_BY_SDK = 3
+# arm constants 
+uint8 ARMED = 1
+uint8 DISARMED = 0
+# gear constants
+uint8 GEAR_UP
+uint8 GEAR_DOWN
+#variables
+uint8 authority
+uint8 armed
+uint8 gear
+uint16 voltage	#volt*100
+time changeTime	 #time of last authority change
---
+++ msg/juk_dji_gps_msg.h
@@ -0,0 +1,10 @@
+float32 lat	#rads
+float32 lng	#rads
+float32 alt	#meters
+float32 vx	#meters/sec
+float32 vy	#meters/sec
+float32 vz	#meters/sec
+float32 course   #rads
+uint8 satellites
+uint8 quality
+time time
